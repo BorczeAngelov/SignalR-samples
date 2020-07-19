e6ee0caf7a9423661ee0ab270988609170589486
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-19 08:40:25 +0200

    WPF client can connect and communicate with service

--- ChatSample/ChatClient.WPF/MainWindow.xaml.cs
+++ ChatSample/ChatClient.WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,56 @@ namespace ChatClient.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly HubConnection _connection;
+
+        public ObservableCollection<string> Messages { get; } = new ObservableCollection<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            _connection = new HubConnectionBuilder()
+               .WithUrl("https://localhost:44340/chat")
+               .Build();
+        }
+
+
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            _connection.On<string, string>("broadcastMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    Messages.Add(newMessage);
+                });
+            });
+
+            try
+            {
+                await _connection.StartAsync();
+                Messages.Add("Connection started");
+                connectButton.IsEnabled = false;
+                sendButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                Messages.Add(ex.Message);
+            }
+        }
+
+        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await _connection.InvokeAsync("Send",
+                    userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                Messages.Add(ex.Message);
+            }
         }
     }
 }
